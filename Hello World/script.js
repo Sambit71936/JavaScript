e68ebe2663c2d903ed2165a5b1ec6a5276f36058
Sambit71936 <sambitjena438@gmail.com>
2025-01-24 21:04:15 +0530

    First File

---
+++ Hello World/script.js
@@ -0,0 +1,10 @@
+alert("Hello World");
+console.log("I am the console")
+console.log("It will show in the console")
+console.log("Code....")
+
+var a = prompt("enter a number")
+// It will ask an input from the user
+
+console.log("Your number " + a)
+// It will show the number in the console and a is the variable
