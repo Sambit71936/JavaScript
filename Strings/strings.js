d6c2e9cd5fa6c77ae30694a206e56af36bb7e913
Sambit71936 <sambitjena438@gmail.com>
2025-01-29 20:07:29 +0530

    Strings

---
+++ Strings/strings.js
@@ -0,0 +1,34 @@
+console.log("Strings");
+
+let a = "Sambit";
+console.log(a);
+console.log(a[0]);// To get the first character of the string
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a[5]);
+
+console.log(a.length)// to get the length of the string
+
+let b ="raj"
+
+console.log("my name is " + a + " and my friend name is " + b);
+
+console.log(`His name is ${a} and my friend name is ${b}`);
+
+let c="Shiva"
+console.log(c.toUpperCase());
+console.log(c.toLowerCase())
+
+
+console.log(c.slice(1,4));
+console.log(c.slice(2));//to get the string from the 2nd index to the end of the string
+console.log(c.replace("Sh", "7"));// to replace the string
+
+console.log(c.concat(a));
+console.log(c.concat(a, "ghfsd", "hksdfgkjh"));
+
+let d="     Hjg";
+d.trim() // to remove the spaces from the string
+
