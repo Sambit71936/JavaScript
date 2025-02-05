d72485fd3ff6596b8736922521bec9409cd3a52a
Sambit71936 <sambitjena438@gmail.com>
2025-02-05 19:33:48 +0530

    DOM

---
+++ DOM - Searching/script.js
@@ -0,0 +1,11 @@
+console.log("Hello World");
+
+let a = document.getElementsByClassName("box");
+console.log(a);
+
+// a[3].style.backgroundColor = "cyan"
+
+let b = document.getElementById("we");
+
+b.style.backgroundColor = "green";
+
