2eb862bf024995941664dc9215bc833429decea6
Sambit71936 <sambitjena438@gmail.com>
2025-02-05 19:50:38 +0530

    DOM

--- DOM - Searching/script.js
+++ DOM - Searching/script.js
@@ -3,9 +3,18 @@ console.log("Hello World");
 let a = document.getElementsByClassName("box");
 console.log(a);
 
-// a[3].style.backgroundColor = "cyan"
+a[3].style.backgroundColor = "cyan"
 
 let b = document.getElementById("we");
 
 b.style.backgroundColor = "green";
 
+document.querySelector(".box").style.backgroundColor = "green"
+
+console.log(document.querySelector(".box"));
+
+document.querySelectorAll(".box").forEach(e =>{
+    e.style.backgroundColor = "blue";
+})
+
+document.getElementsByTagName("div");
