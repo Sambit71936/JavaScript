2d146ece017f5822d7f943d8aecee0033508eef1
Sambit71936 <sambitjena438@gmail.com>
2025-03-07 21:16:48 +0530

    Callbacks and Promises

---
+++ Callbacks and Promises/script.js
@@ -0,0 +1,13 @@
+console.log("a,sfbajsdfvb")
+console.log("khgkgerskbnvsbuyer")
+
+
+setTimeout(() => {
+    console.log("Inside the Timeout 1");
+}, 4000);
+
+setTimeout(() => {
+    console.log("Inside the Timeout 2");
+}, 3000);
+
+console.log("End");
