fb484a6c17b0a7b46fb12e9a83a7efecf76f500f
Sambit71936 <sambitjena438@gmail.com>
2025-03-08 12:02:49 +0530

    Callbacks and Promises

--- Callbacks and Promises/script.js
+++ Callbacks and Promises/script.js
@@ -11,3 +11,17 @@ setTimeout(() => {
 }, 3000);
 
 console.log("End");
+
+const callback = (arg) => {
+  console.log(arg);
+}
+
+
+const loadscript = (src, callback) => {
+    let sc = document.createElement("script");
+    sc.src = src;
+    sc.onload = callback("Hello");
+    document.head.append(sc);
+}
+
+loadscript("https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js", callback);   
